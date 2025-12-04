72a05d1619cfce11c6132f01c64ef6698dcf7984
NighelG <nighel0824@gmail.com>
2025-12-04 13:44:17 -0600

    arreglo pequeño

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext.jsx';
 import Routing from './routes/Routing.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Toaster } from "react-hot-toast"
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
+        <Toaster position="top-right" />
         <Routing />
       </BrowserRouter>
     </AuthProvider>
